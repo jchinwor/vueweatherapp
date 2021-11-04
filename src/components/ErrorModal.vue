a1f0290248bc9a74c1fc53f3ca3691d1f85f369a
Jenkins <90245242+jchinwor@users.noreply.github.com>
2021-11-04 14:01:17 +0000

    Update ErrorModal.vue

--- src/components/ErrorModal.vue
+++ src/components/ErrorModal.vue
@@ -2,11 +2,7 @@
       <div class="flex  justify-center mx-auto">
                <div class="py-3 px-5  mb-2 bg-red-100 text-red-900 text-sm rounded-md border border-red-200 flex items-center justify-between" role="alert">
                         <span class="px-4">Invalid city name! </span>
-                        <!-- <button class="w-4" type="button" data-dismiss="alert" aria-label="Close" onclick="this.parentElement.remove();">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button> -->
+                     
                     </div>
         </div>
 </template>
